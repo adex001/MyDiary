76a3eadaba7a349c5fd06cc266b68eb632f3e0d4
Olatunbosun Adeoye <adex001@gmail.com>
2018-07-26 19:01:32 +0100

    Made update to auth Controller, implementing its functionality

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -10,40 +10,36 @@ dotenv.config();
 class AuthController {
   static login(req, res) {
     // Get the details of user from req body
+    const { email, plainPassword } = req.body;
     // USERS
-    const users = [
-      {
-        email: 'adeoye',
-        password: 'password',
-      },
-      {
-        email: 'testing',
-        password: 'tester',
-      }];
-    const { email, password } = req.body;
     // Find an email by searching through the database
-    const checkEmail = userObject => userObject.email === email;
-    const user = users.find(checkEmail);
-
-    if (user) {
-      // Validate with password
-      if (user.email === email && user.password === password) {
-        console.log('Validation Successful');
-
-        // Attempting to create a token
-        const loginToken = TokenHandler.createToken(user);
-
-        return res.status(200).json({
-          message: 'Token created',
-          loginToken,
+    pool.query(`SELECT * from users WHERE email = '${email}'`, (err, result) => {
+      // If email doesnt exist, return users not found or login failed
+      if (err) {
+        return res.status(404).json({
+          message: 'Login failed! No connection to database.',
         });
       }
-      return res.status(400).json({
-        message: 'Validation failed',
+
+      if (result.rowCount < 1) {
+        return res.status(401).json({
+          message: 'Login failed! No such email',
+        });
+      }
+      // User exists, go ahead to validate password
+      const isCorrectPassword = bcrypt.compareSync(plainPassword, result.rows[0].password);
+      if (!isCorrectPassword) {
+        // if it is not a correct password, return error incorrect parameters
+        return res.status(401).json({
+          message: 'Login failed!',
+        });
+      }
+      // else Attempt to create a token and authenticate
+      const loginToken = TokenHandler.createToken({ id: result.rows[0].userId });
+      return res.status(200).json({
+        message: 'Token created',
+        loginToken,
       });
-    }
-    return res.status(404).json({
-      message: 'User was not found',
     });
   }
 
@@ -63,6 +59,7 @@ class AuthController {
 
     // Insert object into database
     pool.query(`INSERT INTO users (username, email, password, firstname, lastname) VALUES ( '${username}', '${email}', '${password}', '${firstname}', '${lastname}');`, (err, result) => {
+      pool.end(); // returns the pool back.
       if (err) {
         console.log(err);
         return res.status(500).json({
